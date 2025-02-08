d4fa2a0d7587beeec17fc1effaf72a295a3afdc6
enyichiaagu <enyichiaagu@gmail.com>
2025-02-08 13:04:12 +0100

    Added query for fetching product by id

--- products/products.model.js
+++ products/products.model.js
@@ -7,4 +7,16 @@ function getAllProducts() {
   return products;
 }
 
-export { getAllProducts };
+function getProductsByPrice(min, max) {
+  return products.filter((product) => {
+    return product.price >= min && product.price <= max;
+  });
+}
+
+function getProductById(id) {
+  return products.find((product) => {
+    return product.id === id;
+  });
+}
+
+export { getAllProducts, getProductsByPrice, getProductById };
